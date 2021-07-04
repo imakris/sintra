a0e83fd60f3aa3b290d87bdd6b5cede24da6b6b0
Ioannis Makris <imak@imak.gr>
2021-07-05 01:53:34 +0200

    updated the examples for the new library interface

--- example/sintra/sintra_example_2.cpp
+++ example/sintra/sintra_example_2.cpp
@@ -1,13 +1,19 @@
 //
-// Sintra library, example #3
+// Sintra library, example 2
 //
-// This example introduces Transceivers and RPC and demonstrates the following library features:
+// This example introduces Transceivers and RPC and demonstrates
+// the following library features:
 // - how member functions of Transceiver derivatives can be exported for RPC
 // - how to assign a name on the object, which is usable across processes
-// - how to make the remote call of the exported method, using the known name of the object
+// - how to make the remote call of the exported method, using the known
+//   name of the object
+// - RPC failure and interprocess exceptions
 //
-// In this example, Remotely_accessible is a struct defined to offer an 'append' method,
-// which takes a string and an integer and returns another string with its arguments concatenated.
+// In this example, Remotely_accessible is a struct defined to offer an
+// 'append' method, which takes a string and an integer and returns another
+// string with its arguments concatenated. If the supplied string exceeds
+// 10 characters, it throws an exception, which is then transferred to the
+// calling process.
 // This method is exported for RPC.
 // An instance of Remotely_accessible is local to process_1, and used locally.
 // This same instance is used by process_2 remotely.
@@ -20,15 +26,37 @@ using namespace std;
 using namespace sintra;
 
 
+
 struct Remotely_accessible: Derived_transceiver<Remotely_accessible>
 {
     string append(const string& s, int v)
     {
-        return s + to_string(v);
+        if (s.size() > 10) {
+            throw std::logic_error("string too long");
+        }
+        else
+        if (v == 2020) {
+            // crash
+            *((int*)0) = 0;
+        }
+        return to_string(v) + ": " + s;
     }
 
     SINTRA_RPC(append)
+    //SINTRA_RPC_ONLY(append)
 };
+
+
+
+void print_city_year(const char* city, int year)
+{
+    try {
+        console() << Remotely_accessible::rpc_append("instance name", city, year) << "\n";
+    }
+    catch (std::exception& e) {
+        console() << "An RPC exception was thrown: " << e.what() << "\n";
+    }
+}
 
 
 
@@ -36,17 +64,22 @@ int process_1()
 {
     Remotely_accessible ra;
 
-    ra.assign_name("some name");
+    ra.assign_name("instance name");
 
-    // ensure that the instance has been named before trying to access it from another process
-    barrier();
+    // ensure that the instance has been named before attempting
+    // to access it from another process
+    barrier("1st barrier");
 
-    string test_string = ra.append("sydney_", 2000);
 
-    console() << test_string << "\n";
+    console() << ra.append("Sydney", 2000) << "\n";
+
+    // The RPC mechanism may as well be used with local Transceiver instances.
+    // Depending on how the method is exported, it may either resort to a local call,
+    // or be forced to go through the interprocess communication rings.
+    print_city_year("Athens", 2004);
 
     // ensure that ra still exists
-    barrier();
+    barrier("2nd barrier");
     return 0;
 }
 
@@ -54,29 +87,29 @@ int process_1()
 
 int process_2()
 {
-    // ensure that the object has been named before trying to access it with its name
-    barrier();
+    // ensure that the object has been named before attempting
+    // to access it with its name
+    barrier("1st barrier");
 
-    string test_string = Remotely_accessible::rpc_append("some name", "beijing_", 2008);
-    
+    string test_string;
+    print_city_year("Beijing", 2008);
+    print_city_year("Rio de Janeiro", 2016);
+    print_city_year("Tokyo", 2020);
+    print_city_year("Paris", 2024);
+
     // ensure that the remotely accessible instance still exists
-    barrier();
+    barrier("2nd barrier");
 
-    console() << test_string << "\n";
+    //console() << test_string << "\n";
 
     return 0;
 }
 
 
-
-
 int main(int argc, char* argv[])
 {
-    start(
-        argc, argv,
-        Process_descriptor(process_1)
-    ,   Process_descriptor(process_2)
-    );
+    init(argc, argv, process_1, process_2);
+    finalize();
 
     if (process_index() == 0) {
         do {
--- example/sintra/sintra_example_3.cpp
+++ example/sintra/sintra_example_3.cpp
@@ -1,49 +1,51 @@
 //
-// Sintra library, example #4
+// Sintra library, example 3
 //
 // This example demonstrates message dispatch, within the same process.
 // It is very similar to example #1.
 //
-// In this example, there is only one process, with 3 registered slots. Two of them are just
-// simply replying with a message of the opposite type of what they are handling, and the thrd is
-// counting the cycles.
+// In this example, there is only one process, with 3 registered slots.
+// Two of them reply with a message of the opposite type of what they are
+// handling. The third is an observer that is counting the cycles.
 // 
-// Removing the console messages in the ping-pong processes should normally have a substantial
-// effect in performance (in this example, they are commented out).
+// Removing the console messages in the ping-pong processes should
+// normally have a substantial effect in performance (in this example,
+// they are commented out).
 //
 
 #include <sintra/sintra.h>
 #include <iostream>
-#include <omp.h>
 
 
 using namespace std;
 using namespace sintra;
 
 
-
 struct Ping {};
 struct Pong {};
 
+static double timeout_in_seconds = 10.0;
 
-int entry_function()
+int main(int argc, char* argv[])
 {
-    auto ping_slot = [] (Ping) {
+    init(argc, argv);
+
+    auto ping_slot = [](Ping) {
         //console() << "received ping, sending pong \n";
         world() << Pong();
     };
 
-    auto pong_slot = [] (Pong) {
+    auto pong_slot = [](Pong) {
         //console() << "received pong, sending ping \n";
         world() << Ping();
     };
 
-    double ts = omp_get_wtime();
+    double ts = get_wtime();
     double next_ts = ts + 1.;
     uint64_t counter = 0;
 
-    auto benchmark_slot = [&] (Ping) {
-        double ts = omp_get_wtime();
+    auto benchmark_slot = [&](Ping) {
+        double ts = get_wtime();
         if (ts > next_ts) {
             next_ts = ts + 1.;
             console() << counter << " ping-pongs / second\n";
@@ -59,15 +61,9 @@ int entry_function()
     // the spark
     world() << Ping();
 
-    wait_for_stop();
-    return 0;
-}
+    std::this_thread::sleep_for(std::chrono::duration<double>(timeout_in_seconds));
 
-
-
-int main(int argc, char* argv[])
-{
-    start(argc, argv, entry_function);
+    finalize();
 
     return 0;
 }
