24bd0b5f771c6d7146bbad4908043939f7ee1b8e
Ioannis Makris <imak@imak.gr>
2021-07-20 13:35:13 +0200

    updated example comments

--- example/sintra/sintra_example_2.cpp
+++ example/sintra/sintra_example_2.cpp
@@ -1,7 +1,7 @@
 //
 // Sintra library, example 2
 //
-// This example introduces Transceivers and RPC and demonstrates
+// This example introduces Transceivers, RPC and demonstrates
 // the following library features:
 // - how member functions of Transceiver derivatives can be exported for RPC
 // - how to assign a name on the object, which is usable across processes
@@ -11,12 +11,33 @@
 //
 // In this example, Remotely_accessible is a struct defined to offer an
 // 'append' method, which takes a string and an integer and returns another
-// string with its arguments concatenated. If the supplied string exceeds
-// 10 characters, it throws an exception, which is then transferred to the
-// calling process.
-// This method is exported for RPC.
-// An instance of Remotely_accessible is local to process_1, and used locally.
-// This same instance is used by process_2 remotely.
+// string with its arguments concatenated.
+// If the supplied string exceeds 10 characters, it throws an exception,
+// which is then transferred to the calling process.
+// If the integer is 2020, the program should crash, which, to the other
+// processes of the swarm, is communicated with an RPC failure exception
+// and an abnormal termination signal. The latter is not covered in this
+// example.
+// Finally, attempting to call a non-existent instance (which will also be
+// the case if the process it lived in has crashed) will cause another
+// exception.
+// 
+// A Remotely_accessible instance is created and used locally in process_1,
+// and remotely in process_2.
+// 
+// The program could output:
+// 
+// 2000: Sydney
+// 2004: Athens
+// 2008: Beijing
+// An RPC exception was thrown: string too long
+// An RPC exception was thrown: RPC failed
+// An RPC exception was thrown: Attempted to make an RPC call using an invalid instance ID.
+//
+// nevertheless, the output is not really deterministic, as it depends on the
+// timing of the call that will cause process_1 to crash. I.e. 'Sydney' and
+// 'Athens' might theoretically not show at all.
+//
 
 #include <sintra/sintra.h>
 #include <iostream>
@@ -42,7 +63,25 @@ struct Remotely_accessible: Derived_transceiver<Remotely_accessible>
         return to_string(v) + ": " + s;
     }
 
+    // The following statement exports the member function 'append' for RPC use.
+    // A static function 'rpc_append' is defined here, which could be called as follows:
+    //
+    //     Remotely_accessible::rpc_append(instance_id, some_string, some_int);
+    // 
+    // when the instance id is known, or
+    // 
+    //     Remotely_accessible::rpc_append("instance name", some_string, some_int);
+    //
+    // when the instance is named. The latter requires "instance name" to be resolved
+    // to the actual instance id, which will internally cause another interprocess call
+    // the first time this resolution takes place. The id will then be cached locally.
+    // If the instance is local to the process, the call will bypass the RPC mechanism
+    // and resort to a local function call.
     SINTRA_RPC(append)
+
+    // This statement will have a similar effect, with the difference that the function
+    // will always use the RPC mechanism, regardless of whether the instance is local to
+    // the process.
     //SINTRA_RPC_ONLY(append)
 };
 
@@ -70,12 +109,10 @@ int process_1()
     // to access it from another process
     barrier("1st barrier");
 
-
-    console() << ra.append("Sydney", 2000) << "\n";
-
     // The RPC mechanism may as well be used with local Transceiver instances.
     // Depending on how the method is exported, it may either resort to a local call,
     // or be forced to go through the interprocess communication rings.
+    print_city_year("Sydney", 2000);
     print_city_year("Athens", 2004);
 
     // ensure that ra still exists
@@ -99,8 +136,6 @@ int process_2()
 
     // ensure that the remotely accessible instance still exists
     barrier("2nd barrier");
-
-    //console() << test_string << "\n";
 
     return 0;
 }
