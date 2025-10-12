ed82cc62725c08f362f2fc8819bd54eb45e5ff7b
Ioannis Makris <imak@imak.gr>
2025-10-12 19:32:44 +0200

    Fix Windows message id handling

--- include/sintra/detail/utility.h
+++ include/sintra/detail/utility.h
@@ -37,6 +37,12 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 #ifdef _WIN32
+    #ifndef NOMINMAX
+        #define NOMINMAX
+    #endif
+    #ifndef WIN32_LEAN_AND_MEAN
+        #define WIN32_LEAN_AND_MEAN
+    #endif
     #include <windows.h>
     #include <process.h>
 #else
