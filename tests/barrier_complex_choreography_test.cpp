ce92e204a6f8b07d1c0690116d619663f042db05
Ioannis Makris <imak@imak.gr>
2025-10-23 17:21:30 +0200

    Guard delay fuzzing state in complex barrier test

--- tests/barrier_complex_choreography_test.cpp
+++ tests/barrier_complex_choreography_test.cpp
@@ -306,7 +306,8 @@ int coordinator_process()
     std::size_t iterations_completed = 0;
 
     for (std::uint32_t iteration = 0; iteration < kIterations; ++iteration) {
-        sintra::set_delay_fuzzing_run_index(iteration);
+        sintra::set_delay_fuzzing_seed(iteration);
+        const sintra::detail::delay_fuzz_scope fuzz_scope{true};
         const auto start_barrier = make_iteration_barrier_name("complex-start", iteration);
         barrier(start_barrier);
 
@@ -363,7 +364,8 @@ int coordinator_process()
     {
         std::lock_guard<std::mutex> lock(state.mutex);
         for (std::uint32_t iteration = 0; iteration < kIterations; ++iteration) {
-            sintra::set_delay_fuzzing_run_index(iteration);
+            sintra::set_delay_fuzzing_seed(iteration);
+            const sintra::detail::delay_fuzz_scope fuzz_scope{true};
             stage_a_total += state.stage_counts[0][iteration];
             stage_b_total += state.stage_counts[1][iteration];
         }
@@ -436,7 +438,8 @@ int stage_process(std::uint32_t stage, std::uint32_t worker_index)
     };
 
     for (std::uint32_t iteration = 0; iteration < kIterations; ++iteration) {
-        sintra::set_delay_fuzzing_run_index(iteration);
+        sintra::set_delay_fuzzing_seed(iteration);
+        const sintra::detail::delay_fuzz_scope fuzz_scope{true};
         const auto start_barrier = make_iteration_barrier_name("complex-start", iteration);
         barrier(start_barrier);
 
