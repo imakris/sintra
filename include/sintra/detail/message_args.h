8211fa0848f96e93f4da00751fd37d66e576d227
Ioannis Makris <imak@imak.gr>
2025-10-26 19:31:39 +0100

    Fix message_args traits to strip references

--- include/sintra/detail/message_args.h
+++ include/sintra/detail/message_args.h
@@ -10,6 +10,10 @@ namespace sintra {
 namespace detail {
 
 template <typename T>
+using remove_cvref_t = typename std::remove_cv<
+    typename std::remove_reference<T>::type>::type;
+
+template <typename T>
 using message_arg_storage_value_t = std::conditional_t<
     std::is_reference<T>::value,
     std::reference_wrapper<typename std::remove_reference<T>::type>,
@@ -75,7 +79,7 @@ struct message_args
 
 template <typename T>
 struct message_args_size
-    : message_args_size<typename std::remove_cv<T>::type::message_args_type>
+    : message_args_size<typename remove_cvref_t<T>::message_args_type>
 {};
 
 template <typename... Args>
@@ -85,7 +89,7 @@ struct message_args_size<message_args<Args...>>
 
 template <typename T, std::size_t I>
 struct message_args_nth_type
-    : message_args_nth_type<typename std::remove_cv<T>::type::message_args_type, I>
+    : message_args_nth_type<typename remove_cvref_t<T>::message_args_type, I>
 {};
 
 template <std::size_t I, typename... Args>
--- tests/dummy_test.cpp
+++ tests/dummy_test.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
+#include <type_traits>
+
+#include <sintra/detail/message_args.h>
+
+namespace {
+
+using args_t = sintra::detail::message_args<int, double>;
+
+static_assert(
+    sintra::detail::message_args_size<args_t&>::value == 2,
+    "message_args_size should ignore references");
+static_assert(
+    sintra::detail::message_args_size<const args_t&>::value == 2,
+    "message_args_size should ignore cv-qualified references");
+static_assert(
+    std::is_same<
+        double,
+        sintra::detail::message_args_nth_type<args_t&, 1>::type>::value,
+    "message_args_nth_type should ignore references");
+
+} // namespace
 
 int main() {
     std::cout << "Dummy test executed successfully." << std::endl;
