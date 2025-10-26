eb1320c2f3d3a2a104328bbce4e6f630e9473128
Ioannis Makris <imak@imak.gr>
2025-10-26 12:19:58 +0100

    Replace Boost type index with RTTI and add ABI guard

---
+++ include/sintra/detail/type_utils.h
@@ -0,0 +1,222 @@
+// Copyright (c) 2025, Ioannis Makris
+// Licensed under the BSD 2-Clause License, see LICENSE.md file for details.
+
+#pragma once
+
+#include <cstdlib>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <typeinfo>
+
+#if defined(__GNUG__)
+#include <cxxabi.h>
+#endif
+
+namespace sintra::detail {
+
+namespace detail_type_utils {
+
+inline std::string demangle(const char* name)
+{
+#if defined(__GNUG__)
+    int status = 0;
+    std::unique_ptr<char, void(*)(void*)> demangled(
+        abi::__cxa_demangle(name, nullptr, nullptr, &status),
+        &std::free);
+    if (status == 0 && demangled) {
+        return demangled.get();
+    }
+#else
+    (void)name;
+#endif
+    return name ? name : std::string{};
+}
+
+inline std::string compiler_identity()
+{
+#if defined(_MSC_VER)
+    std::ostringstream oss;
+    oss << "msvc-" << _MSC_VER;
+    return oss.str();
+#elif defined(__clang__)
+    std::ostringstream oss;
+    oss << "clang-" << __clang_major__ << '.' << __clang_minor__ << '.' << __clang_patchlevel__;
+    return oss.str();
+#elif defined(__GNUC__)
+    std::ostringstream oss;
+    oss << "gnu-" << __GNUC__ << '.' << __GNUC_MINOR__ << '.' << __GNUC_PATCHLEVEL__;
+    return oss.str();
+#elif defined(__INTEL_COMPILER)
+    std::ostringstream oss;
+    oss << "intel-" << __INTEL_COMPILER;
+    return oss.str();
+#else
+    return "unknown-compiler";
+#endif
+}
+
+inline std::string stdlib_identity()
+{
+#if defined(_MSVC_STL_VERSION)
+    std::ostringstream oss;
+    oss << "msvc-stl-" << _MSVC_STL_VERSION;
+    return oss.str();
+#elif defined(_LIBCPP_VERSION)
+    std::ostringstream oss;
+    oss << "libc++-" << _LIBCPP_VERSION;
+    return oss.str();
+#elif defined(__GLIBCXX__)
+    std::ostringstream oss;
+    oss << "libstdc++-" << __GLIBCXX__;
+    return oss.str();
+#else
+    return "unknown-stdlib";
+#endif
+}
+
+inline std::string platform_identity()
+{
+#if defined(__MINGW64__)
+    return "windows-mingw64";
+#elif defined(__MINGW32__)
+    return "windows-mingw";
+#elif defined(_WIN32)
+    return "windows-msvc";
+#elif defined(__APPLE__)
+    return "apple";
+#elif defined(__linux__)
+    return "linux";
+#elif defined(__FreeBSD__)
+    return "freebsd";
+#elif defined(__NetBSD__)
+    return "netbsd";
+#elif defined(__OpenBSD__)
+    return "openbsd";
+#elif defined(__EMSCRIPTEN__)
+    return "emscripten";
+#else
+    return "unknown-platform";
+#endif
+}
+
+inline std::string architecture_identity()
+{
+#if defined(__x86_64__) || defined(_M_X64)
+    return "x86_64";
+#elif defined(__i386__) || defined(_M_IX86)
+    return "x86";
+#elif defined(__aarch64__) || defined(_M_ARM64)
+    return "aarch64";
+#elif defined(__arm__) || defined(_M_ARM)
+    return "arm";
+#elif defined(__ppc64__) || defined(__powerpc64__)
+    return "ppc64";
+#elif defined(__ppc__) || defined(__powerpc__)
+    return "ppc";
+#elif defined(__riscv)
+    return "riscv";
+#else
+    return "unknown-arch";
+#endif
+}
+
+inline std::string describe_token_component(const std::string& component)
+{
+    auto equals = component.find('=');
+    if (equals == std::string::npos) {
+        return component;
+    }
+
+    std::string name = component.substr(0, equals);
+    std::string value = component.substr(equals + 1);
+
+    if (name == "compiler") {
+        return "compiler " + value;
+    }
+    if (name == "stdlib") {
+        return "standard library " + value;
+    }
+    if (name == "platform") {
+        return "platform " + value;
+    }
+    if (name == "arch") {
+        return "architecture " + value;
+    }
+
+    return name + ' ' + value;
+}
+
+} // namespace detail_type_utils
+
+inline std::string type_name_from_info(const std::type_info& info)
+{
+    return detail_type_utils::demangle(info.name());
+}
+
+template <typename T>
+inline std::string type_name()
+{
+    return type_name_from_info(typeid(T));
+}
+
+inline std::string abi_token()
+{
+    std::ostringstream oss;
+    oss << "compiler=" << detail_type_utils::compiler_identity();
+    oss << ";stdlib=" << detail_type_utils::stdlib_identity();
+    oss << ";platform=" << detail_type_utils::platform_identity();
+    oss << ";arch=" << detail_type_utils::architecture_identity();
+    return oss.str();
+}
+
+inline std::string abi_description()
+{
+    std::ostringstream oss;
+    oss << "compiler " << detail_type_utils::compiler_identity();
+    oss << ", standard library " << detail_type_utils::stdlib_identity();
+    oss << ", platform " << detail_type_utils::platform_identity();
+    oss << ", architecture " << detail_type_utils::architecture_identity();
+    return oss.str();
+}
+
+inline std::string describe_abi_token(const std::string& token)
+{
+    if (token.empty()) {
+        return std::string("(unspecified ABI)");
+    }
+
+    std::ostringstream oss;
+    bool first = true;
+    std::size_t start = 0;
+    while (start < token.size()) {
+        auto next = token.find(';', start);
+        auto component = token.substr(start, next == std::string::npos ? std::string::npos : next - start);
+        auto readable = detail_type_utils::describe_token_component(component);
+        if (!readable.empty()) {
+            if (!first) {
+                oss << ", ";
+            }
+            oss << readable;
+            first = false;
+        }
+        if (next == std::string::npos) {
+            break;
+        }
+        start = next + 1;
+    }
+
+    auto description = oss.str();
+    if (description.empty()) {
+        return token;
+    }
+    return description;
+}
+
+inline const char* abi_marker_filename()
+{
+    return "sintra_abi.info";
+}
+
+} // namespace sintra::detail
+
