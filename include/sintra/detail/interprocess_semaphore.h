f3181bd83ca26c1337ef5c9953664274d5cabf65
Ioannis Makris <imak@imak.gr>
2025-10-27 23:17:49 +0100

    Enable os_sync_wait_on_address on macOS

--- include/sintra/detail/interprocess_semaphore.h
+++ include/sintra/detail/interprocess_semaphore.h
@@ -2,6 +2,7 @@
 
 #include <atomic>
 #include <chrono>
+#include <cstddef>
 #include <cstdint>
 #include <stdexcept>
 #include <system_error>
@@ -23,21 +24,52 @@
 #elif defined(__APPLE__)
   #include <cerrno>
   #include <ctime>
-  #if defined(__has_include)
-    #if __has_include(<os/os_sync_wait_on_address.h>) && __has_include(<os/clock.h>)
-      #include <os/os_sync_wait_on_address.h>
-      #include <os/clock.h>
-      #define SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS 1
+
+  #if defined(__has_include) && __has_include(<os/clock.h>)
+    #include <os/clock.h>
+  #else
+    #include <time.h>
+    using os_clockid_t = clockid_t;
+    #ifndef OS_CLOCK_REALTIME
+      #define OS_CLOCK_REALTIME CLOCK_REALTIME
     #endif
   #endif
-  #if !defined(SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS)
-    #define SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS 0
-    #include <fcntl.h>
-    #include <semaphore.h>
-    #include <mutex>
-    #include <unordered_map>
-    #include <cstdio>
+
+  #if defined(__has_include) && __has_include(<os/os_sync_wait_on_address.h>)
+    #include <os/os_sync_wait_on_address.h>
+  #else
+    using os_sync_wait_on_address_flags_t = uint32_t;
+    using os_sync_wake_by_address_flags_t = uint32_t;
+
+    constexpr os_sync_wait_on_address_flags_t OS_SYNC_WAIT_ON_ADDRESS_NONE = 0x00000000u;
+    constexpr os_sync_wait_on_address_flags_t OS_SYNC_WAIT_ON_ADDRESS_SHARED = 0x00000001u;
+    constexpr os_sync_wake_by_address_flags_t OS_SYNC_WAKE_BY_ADDRESS_NONE = 0x00000000u;
+    constexpr os_sync_wake_by_address_flags_t OS_SYNC_WAKE_BY_ADDRESS_SHARED = 0x00000001u;
+
+    extern "C"
+    {
+        int os_sync_wait_on_address(void* addr,
+                                    uint64_t value,
+                                    size_t size,
+                                    os_sync_wait_on_address_flags_t flags);
+
+        int os_sync_wait_on_address_with_timeout(void* addr,
+                                                 uint64_t value,
+                                                 size_t size,
+                                                 os_sync_wait_on_address_flags_t flags,
+                                                 os_clockid_t clockid,
+                                                 uint64_t timeout_ns);
+
+        int os_sync_wake_by_address_any(void* addr,
+                                        size_t size,
+                                        os_sync_wake_by_address_flags_t flags);
+
+        int os_sync_wake_by_address_all(void* addr,
+                                        size_t size,
+                                        os_sync_wake_by_address_flags_t flags);
+    }
   #endif
+
 #else
   #include <cerrno>
   #include <semaphore.h>
@@ -137,61 +169,6 @@ namespace interprocess_semaphore_detail
             ::CloseHandle(handle);
         }
     }
-#elif defined(__APPLE__) && !SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS
-    inline std::mutex& handle_mutex()
-    {
-        static std::mutex mtx;
-        return mtx;
-    }
-
-    inline std::unordered_map<uint64_t, sem_t*>& handle_map()
-    {
-        static std::unordered_map<uint64_t, sem_t*> map;
-        return map;
-    }
-
-    inline sem_t* register_handle(uint64_t id, sem_t* handle)
-    {
-        std::lock_guard<std::mutex> lock(handle_mutex());
-        handle_map()[id] = handle;
-        return handle;
-    }
-
-    inline sem_t* ensure_handle(uint64_t id, const char* name)
-    {
-        {
-            std::lock_guard<std::mutex> lock(handle_mutex());
-            auto it = handle_map().find(id);
-            if (it != handle_map().end()) {
-                return it->second;
-            }
-        }
-
-        sem_t* sem = sem_open(name, 0);
-        if (sem == SEM_FAILED) {
-            throw std::system_error(errno, std::generic_category(), "sem_open");
-        }
-
-        std::lock_guard<std::mutex> lock(handle_mutex());
-        return handle_map().emplace(id, sem).first->second;
-    }
-
-    inline void close_handle(uint64_t id)
-    {
-        sem_t* sem = nullptr;
-        {
-            std::lock_guard<std::mutex> lock(handle_mutex());
-            auto it = handle_map().find(id);
-            if (it != handle_map().end()) {
-                sem = it->second;
-                handle_map().erase(it);
-            }
-        }
-
-        if (sem) {
-            while (sem_close(sem) == -1 && errno == EINTR) {}
-        }
-    }
 #endif
 }
 
@@ -202,10 +179,8 @@ public:
     {
 #if defined(_WIN32)
         initialise_windows(initial_count);
-#elif defined(__APPLE__) && SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS
-        initialise_os_sync(initial_count);
 #elif defined(__APPLE__)
-        initialise_named(initial_count);
+        initialise_os_sync(initial_count);
 #else
         initialise_posix(initial_count);
 #endif
@@ -218,10 +193,8 @@ public:
     {
 #if defined(_WIN32)
         teardown_windows();
-#elif defined(__APPLE__) && SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS
-        teardown_os_sync();
 #elif defined(__APPLE__)
-        teardown_named();
+        teardown_os_sync();
 #else
         teardown_posix();
 #endif
@@ -231,10 +204,8 @@ public:
     {
 #if defined(_WIN32)
         interprocess_semaphore_detail::close_handle(m_windows.id);
-#elif defined(__APPLE__) && SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS
-        // Nothing to do for the os_sync based implementation.
 #elif defined(__APPLE__)
-        interprocess_semaphore_detail::close_handle(m_named.id);
+        // Nothing to do for the os_sync based implementation.
 #else
         // Nothing to do for POSIX unnamed semaphores.
 #endif
@@ -247,16 +218,8 @@ public:
         if (!::ReleaseSemaphore(handle, 1, nullptr)) {
             throw std::system_error(::GetLastError(), std::system_category(), "ReleaseSemaphore");
         }
-#elif defined(__APPLE__) && SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS
-        post_os_sync();
 #elif defined(__APPLE__)
-        sem_t* sem = named_handle();
-        while (sem_post(sem) == -1) {
-            if (errno == EINTR) {
-                continue;
-            }
-            throw std::system_error(errno, std::generic_category(), "sem_post");
-        }
+        post_os_sync();
 #else
         while (sem_post(&m_sem) == -1) {
             if (errno == EINTR) {
@@ -275,16 +238,8 @@ public:
         if (result != WAIT_OBJECT_0) {
             throw std::system_error(::GetLastError(), std::system_category(), "WaitForSingleObject");
         }
-#elif defined(__APPLE__) && SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS
-        wait_os_sync();
 #elif defined(__APPLE__)
-        sem_t* sem = named_handle();
-        while (sem_wait(sem) == -1) {
-            if (errno == EINTR) {
-                continue;
-            }
-            throw std::system_error(errno, std::generic_category(), "sem_wait");
-        }
+        wait_os_sync();
 #else
         while (sem_wait(&m_sem) == -1) {
             if (errno == EINTR) {
@@ -307,20 +262,8 @@ public:
             return false;
         }
         throw std::system_error(::GetLastError(), std::system_category(), "WaitForSingleObject");
-#elif defined(__APPLE__) && SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS
-        return try_acquire_os_sync();
 #elif defined(__APPLE__)
-        sem_t* sem = named_handle();
-        while (sem_trywait(sem) == -1) {
-            if (errno == EINTR) {
-                continue;
-            }
-            if (errno == EAGAIN) {
-                return false;
-            }
-            throw std::system_error(errno, std::generic_category(), "sem_trywait");
-        }
-        return true;
+        return try_acquire_os_sync();
 #else
         while (sem_trywait(&m_sem) == -1) {
             if (errno == EINTR) {
@@ -364,7 +307,7 @@ public:
             return false;
         }
         throw std::system_error(::GetLastError(), std::system_category(), "WaitForSingleObject");
-#elif defined(__APPLE__) && SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS
+#elif defined(__APPLE__)
         int32_t previous = m_os_sync.count.fetch_sub(1, std::memory_order_acq_rel);
         if (previous > 0) {
             return true;
@@ -460,7 +403,7 @@ private:
     {
         interprocess_semaphore_detail::close_handle(m_windows.id);
     }
-#elif defined(__APPLE__) && SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS
+#elif defined(__APPLE__)
     struct os_sync_storage
     {
         std::atomic<int32_t> count{0};
@@ -609,44 +552,6 @@ private:
             throw std::system_error(errno, std::generic_category(), "os_sync_wake_by_address_any");
         }
     }
-#elif defined(__APPLE__)
-    struct named_storage
-    {
-        uint64_t id = 0;
-        char     name[32]{};
-    };
-
-    named_storage m_named;
-
-    void initialise_named(unsigned int initial_count)
-    {
-        m_named.id = interprocess_semaphore_detail::generate_global_identifier();
-        std::snprintf(m_named.name,
-                      sizeof(m_named.name) / sizeof(m_named.name[0]),
-                      "/sintra_sem_%016llx",
-                      static_cast<unsigned long long>(m_named.id));
-
-        sem_unlink(m_named.name);
-        sem_t* sem = sem_open(m_named.name, O_CREAT | O_EXCL, 0600, initial_count);
-        if (sem == SEM_FAILED) {
-            throw std::system_error(errno, std::generic_category(), "sem_open");
-        }
-
-        interprocess_semaphore_detail::register_handle(m_named.id, sem);
-    }
-
-    sem_t* named_handle() const
-    {
-        return interprocess_semaphore_detail::ensure_handle(m_named.id, m_named.name);
-    }
-
-    void teardown_named() noexcept
-    {
-        interprocess_semaphore_detail::close_handle(m_named.id);
-        if (m_named.name[0] != '\0') {
-            sem_unlink(m_named.name);
-        }
-    }
 #else
     sem_t m_sem{};
 
@@ -666,6 +571,3 @@ private:
 
 } // namespace sintra::detail
 
-#if defined(__APPLE__) && defined(SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS)
-#  undef SINTRA_HAS_OS_SYNC_WAIT_ON_ADDRESS
-#endif
